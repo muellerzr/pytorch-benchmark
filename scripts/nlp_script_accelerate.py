2475aa7cdf3b21a549dbb9449aa9366a199391a1
Zach Mueller <muellerzr@gmail.com>
2022-07-19 10:11:41 -0400

    Move seed change

--- scripts/nlp_script_accelerate.py
+++ scripts/nlp_script_accelerate.py
@@ -96,7 +96,6 @@ def main(
         metric = evaluate.load("glue", "mrpc")
 
         set_seed(seed)
-        seed += 100
         train_dataloader, eval_dataloader = get_dataloaders(accelerator, batch_size, eval_batch_size)
         # Instantiate the model (we build the model here so that the seed also control new weights initialization)
         model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", return_dict=True)
@@ -227,3 +226,4 @@ def main(
             print(f'Report saved to reports/nlp_script_accelerate/{typ}_{fname}/run_{iteration}.json')
         del model, optimizer, train_dataloader, eval_dataloader, lr_scheduler
         clear_memory()
+        seed += 100
